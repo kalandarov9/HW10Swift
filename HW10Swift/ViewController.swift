c52deb31c93095e7179948d2c8eb04e70623b5fd
Odin_9 <kalandarov9@gmail.com>
2021-11-08 00:01:42 +0300

    Initial Commit

---
+++ HW10Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW10Swift
+//
+//  Created by Yuri Kalandarov on 08.11.2021.
+//  Copyright © 2021 Yuri Kalandarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
