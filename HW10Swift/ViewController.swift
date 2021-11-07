01df10b1b95c3759d33deafbcea7444719d2d6d4
Odin_9 <kalandarov9@gmail.com>
2021-11-08 00:16:10 +0300

    bug-fix task 2 deleted comments

--- HW10Swift/ViewController.swift
+++ HW10Swift/ViewController.swift
@@ -1,23 +1,14 @@
-//
-//  ViewController.swift
-//  HW10Swift
-//
-//  Created by Yuri Kalandarov on 08.11.2021.
-//  Copyright © 2021 Yuri Kalandarov. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView (){
            view.backgroundColor = .systemRed
        }
-    
+
 }
 
